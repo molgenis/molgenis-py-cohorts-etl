0a9d76bab1912f4437ba8f82a386fd7e5ca888c9
ypezijlstra <ype@ypezijlstra.frl>
2023-02-28 16:48:46 +0100

    Changed logger info messages to f-strings with job strategies

--- job.py
+++ job.py
@@ -55,28 +55,28 @@ class Job:
     def run_strategy(self) -> None:
 
         if self.job_strategy == JobStrategy.NETWORK_STAGING_TO_DATA_CATALOGUE_ZIP.name:
-            log.info('Run job strategy: ' + self.job_strategy)
+            log.info(f'Run job strategy: {self.job_strategy}')
             sync.Sync.network_zip_to_datacatalogue(self)
         elif self.job_strategy == JobStrategy.COHORT_STAGING_TO_DATA_CATALOGUE_ZIP.name:
-            log.info('Run job strategy: ' + self.job_strategy)
+            log.info(f'Run job strategy: {self.job_strategy}')
             sync.Sync.cohort_zip_to_datacatalogue(self)
         elif self.job_strategy == JobStrategy.UMCG_COHORT_STAGING_TO_DATA_CATALOGUE_ZIP.name:
-            log.info('Run job strategy: ' + self.job_strategy)
+            log.info(f'Run job strategy: {self.job_strategy}')
             sync.Sync.umcg_cohort_zip_to_datacatalogue(self)
         elif self.job_strategy == JobStrategy.FILL_STAGING.name:
-            log.info('Run job strategy: ' + self.job_strategy)
+            log.info(f'Run job strategy: {self.job_strategy}')
             sync.Sync.fill_staging(self)
         elif self.job_strategy == JobStrategy.SHARED_STAGING.name:
-            log.info('Run job strategy: ' + self.job_strategy)
+            log.info(f'Run job strategy: {self.job_strategy}')
             sync.Sync.shared_staging(self)
         elif self.job_strategy == JobStrategy.UMCG_SHARED_ONTOLOGY_ZIP.name:
-            log.info('Run job strategy: ' + self.job_strategy)
+            log.info(f'Run job strategy: {self.job_strategy}')
             sync.Sync.umcg_shared_ontology_zip_to_datacatalogue(self)
         elif self.job_strategy == JobStrategy.ONTOLOGY_STAGING_TO_DATA_CATALOGUE_ZIP.name:
-            log.info('Run job strategy: ' + self.job_strategy)
+            log.info(f'Run job strategy: {self.job_strategy}')
             sync.Sync.ontology_staging_zip_to_datacatalogue(self)
         elif self.job_strategy == JobStrategy.FILL_NETWORK.name:
-            log.info('Run job strategy: ' + self.job_strategy)
+            log.info(f'Run job strategy: {self.job_strategy}')
             sync.Sync.fill_network(self)
         else:
             log.error(f'Job Strategy not set, please use: ')
