28228c543cf05b799ac6d507d9356dd762d95f01
ypezijlstra <ype@ypezijlstra.frl>
2023-03-01 09:29:13 +0100

    Moved tablesToSync, tablesToDelete and ontologieToSync to classes within new constants.py file

--- sync.py
+++ sync.py
@@ -1,4 +1,5 @@
 import util
+from constants import OntologiesToSync, TablesToSync, TablesToDelete
 
 
 class Sync:
@@ -6,44 +7,17 @@ class Sync:
         """ Sync SOURCE (catalogue) with TARGET (staging)
         """       
         # order of tables is important, value equals filter
-        tablesToSync = {
-            'Publications': None,
-            'Cohorts': 'pid',
-            'Documentation': 'resource',
-            'Contributions': 'resource',
-            'Subcohorts': 'resource',
-            'CollectionEvents': 'resource',
-            'Partners': 'resource',
-            'SourceDataDictionaries': 'resource',
-            'SourceTables': 'dataDictionary.resource',
-            'SourceVariables': 'dataDictionary.resource',
-            'RepeatedSourceVariables': 'dataDictionary.resource',
-            'SourceVariableValues': 'dataDictionary.resource',
-            'TableMappings': 'fromDataDictionary.resource',
-            'VariableMappings': 'fromDataDictionary.resource',
-        }
+        tablesToSync = TablesToSync.FILL_STAGING
         util.Util.download_filter_upload(self, tablesToSync)
 
     def shared_staging(self) -> None:
         """ Sync SOURCE (SharedStaging) with TARGET """
-        tablesToSync = {
-            'Institutions': None,
-            'Contacts': None,
-        }
+        tablesToSync = TablesToSync.SHARED_STAGING
         util.Util.download_filter_upload(self, tablesToSync)
 
     def fill_network(self) -> None:
         # order of tables is important, value equals filter
-        tablesToSync = {
-            'Models': 'pid',
-            'TargetDataDictionaries': 'resource',
-            'TargetTables': 'dataDictionary.resource',
-            'Subcohorts': 'resource',
-            'CollectionEvents': 'resource',
-            'TargetVariables': 'dataDictionary.resource',
-            'TargetVariableValues': 'dataDictionary.resource',
-            'RepeatedTargetVariables': 'dataDictionary.resource',
-        }
+        tablesToSync = TablesToSync.FILL_NETWORK
 
         util.Util.download_filter_upload(self, tablesToSync, network=True)
 
@@ -52,29 +26,8 @@ class Sync:
         util.Util.download_target(self)
         
         # Database name needs to be identical to cohort PID
-        tablesToDelete = {
-            # 'Publications': 'doi',
-            'Documentation': 'resource',
-            'Contributions': 'resource',
-            'CollectionEvents': 'resource',
-            'Partners': 'resource',
-            'SubcohortCounts': 'subcohort',
-            'Subcohorts': 'resource',
-            'Cohorts': 'pid',
-        }
-
-        tablesToSync = {
-            'Publications': None,
-            'Documentation': None,
-            'Contributions': None,
-            'CollectionEvents': None,
-            # 'Subcohorts': None,
-            'Partners': None,
-            'SubcohortCounts': None,
-            'Subcohorts': None,
-            'Resources': None,
-            'Cohorts': None,
-        }
+        tablesToDelete = TablesToDelete.UMCG_COHORT_STAGING_TO_DATA_CATALOGUE_ZIP
+        tablesToSync = TablesToSync.UMCG_COHORT_STAGING_TO_DATA_CATALOGUE_ZIP
        
         util.Util.delete_cohort_from_data_catalogue(self, tablesToDelete)
         util.Util.download_zip_process(self, tablesToSync)
@@ -84,57 +37,14 @@ class Sync:
         util.Util.download_target(self)
         
         # Database name needs to be identical to cohort PID
-        tablesToDelete = {
-            'VariableMappings': 'mappings',
-            'TableMappings': 'mappings',
-            'SourceVariableValues': 'variables',
-            'RepeatedSourceVariables': 'variables',
-            'SourceVariables': 'variables',
-            'SourceTables': 'variables',
-            'SourceDataDictionaries': 'resource',
-            'Documentation': 'resource',
-            'Contributions': 'resource',
-            'CollectionEvents': 'resource',
-            'Subcohorts': 'resource',
-            'Partners': 'resource',
-            'Cohorts': 'pid',
-        }
-
-        tablesToSync = {
-            'Publications': None,
-            'VariableMappings': None,
-            'TableMappings': None,
-            'SourceVariableValues': None,
-            'RepeatedSourceVariables': None,
-            'SourceVariables': None,
-            'SourceTables': None,
-            'SourceDataDictionaries': None,
-            'Documentation': None,
-            'Contributions': None,
-            'CollectionEvents': None,
-            'Subcohorts': None,
-            'Partners': None,
-            'Cohorts': None,
-        }
+        tablesToDelete = TablesToDelete.COHORT_STAGING_TO_DATA_CATALOGUE_ZIP
+        tablesToSync = TablesToSync.COHORT_STAGING_TO_DATA_CATALOGUE_ZIP
        
         util.Util.delete_cohort_from_data_catalogue(self, tablesToDelete)
         util.Util.download_zip_process(self, tablesToSync)
     
     def network_zip_to_datacatalogue(self) -> None:
-        tablesToSync = {
-            'AllTargetVariables': None,
-            'CollectionEvents': None,
-            'Models': None,
-            'Networks': None,
-            'RepeatedTargetVariables': None,
-            'Resources': None,
-            'Subcohorts': None,
-            'TargetDataDictionaries': None,
-            'TargetTables': None,
-            'TargetVariables': None,
-            'TargetVariableValues': None,
-
-        }
+        tablesToSync = TablesToSync.NETWORK_STAGING_TO_DATA_CATALOGUE_ZIP
         util.Util.download_zip_process(self, tablesToSync)
     
     def umcg_shared_ontology_zip_to_datacatalogue(self) -> None:
@@ -142,59 +52,12 @@ class Sync:
         Upload CoreVariables to CatalogueOntologies as a zip"""
         util.Util.download_target(self)
 
-        ontologiesToSync = {
-            'CoreVariables': None
-        }
+        ontologiesToSync = OntologiesToSync.UMCG_SHARED_ONTOLOGY_ZIP
         util.Util.download_zip_process(self, ontologiesToSync)
     
     def ontology_staging_zip_to_datacatalogue(self) -> None:
         """ Upload SOURCE CatalogueOntologies to TARGET CatalogueOntologies as a zip"""
         util.Util.download_target(self)
 
-        ontologiesToSync = {
-            'AgeGroups': None,
-            'AreasOfInformation': None,
-            'CareSettings': None,
-            'CohortDesigns': None,
-            'CollectionTypes': None,
-            'ContributionTypes': None,
-            'CoreVariables': None,
-            'Countries': None,
-            'DAPsAccessCompleteness': None,
-            'DAPsAccessLevels': None,
-            'DAPsAccessPermissions': None,
-            'DAPsReasonsForAccess': None,
-            'DataAccessConditions': None,
-            'DataCategories': None,
-            'DataUseConditions': None,
-            'DatabankFamilies': None,
-            'DatasourceTypes': None,
-            'Diseases': None,
-            'DocumentTypes': None,
-            'Formats': None,
-            'InformedConsents': None,
-            'InstitutionRoles': None,
-            'InstitutionTypes': None,
-            'Keywords': None,
-            'Languages': None,
-            'LinkageStrategies': None,
-            'Months': None,
-            'ObservationTargets': None,
-            'PartnerRoles': None,
-            'PopulationEntryCauses': None,
-            'PopulationExitCauses': None,
-            'PopulationSubsets': None,
-            'Regions': None,
-            'ReleaseTypes': None,
-            'ResourceTypes': None,
-            'SampleCategories': None,
-            'StandardizedTools': None,
-            'Status': None,
-            'StatusDetails': None,
-            'StudyTypes': None,
-            'Titles': None,
-            'Units': None,
-            'Vocabularies': None,
-            'Years': None,
-        }
+        ontologiesToSync = OntologiesToSync.ONTOLOGY_STAGING_TO_DATA_CATALOGUE_ZIP
         util.Util.download_zip_process(self, ontologiesToSync)
