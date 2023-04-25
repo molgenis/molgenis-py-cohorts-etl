b892fe50e2857163dffd4e0be8f5039e7135c325
ypezijlstra <ype@ypezijlstra.frl>
2023-04-25 12:33:47 +0200

    Improved run.py readability
    * Replaced uppercase variable names by lowercase names, e.g. 'SOURCE_URL' -> 'source_url'
    * Replaced console output strings by f-strings and improved spacing of output
    * Replaced use of decouple package by dotenv package for reading environment variables

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import logging
 import os
 import sys
 
-from decouple import config
+from dotenv import load_dotenv
 
 from src import Job, JobStrategy
 
@@ -11,53 +11,56 @@ def main():
     logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
     log = logging.getLogger('run')
 
+    # Load the variables from the .env file into the environment
+    load_dotenv()
+
     try:
-        SOURCE_URL = config('MG_SOURCE_URL')
-        SOURCE_USERNAME = config('MG_SOURCE_USERNAME')
-        SOURCE_PASSWORD = config('MG_SOURCE_PASSWORD')
-        SOURCE_DATABASE = config('MG_SOURCE_DATABASE')
+        source_url = os.getenv('MG_SOURCE_URL')
+        source_username = os.getenv('MG_SOURCE_USERNAME')
+        source_password = os.getenv('MG_SOURCE_PASSWORD')
+        source_database = os.getenv('MG_SOURCE_DATABASE')
 
-        TARGET_URL = config('MG_TARGET_URL')
-        TARGET_USERNAME = config('MG_TARGET_USERNAME')
-        TARGET_PASSWORD = config('MG_TARGET_PASSWORD')
-        TARGET_DATABASE = config('MG_TARGET_DATABASE')
+        target_url = os.getenv('MG_TARGET_URL')
+        target_username = os.getenv('MG_TARGET_USERNAME')
+        target_password = os.getenv('MG_TARGET_PASSWORD')
+        target_database = os.getenv('MG_TARGET_DATABASE')
 
-        JOB_STRATEGY = config('MG_JOB_STRATEGY')
+        job_strategy = os.getenv('MG_JOB_STRATEGY')
 
-        isinstance(JobStrategy[JOB_STRATEGY], JobStrategy)
+        isinstance(JobStrategy[job_strategy], JobStrategy)
     except:
         log.error('Make sure you filled in all variables in the .env file, script will exit now.')
         sys.exit()
 
     log.info('*** START SYNC JOB WITH settings: ***')
-    log.info('JOB_STRATEGY:    ' + JOB_STRATEGY)
+    log.info(f'JOB_STRATEGY:       {job_strategy}')
 
-    log.info('TARGET_URL:      ' + TARGET_URL)
-    log.info('TARGET_USERNAME: ' + TARGET_USERNAME)
-    log.info('TARGET_PASSWORD: ********')
-    log.info('TARGET_DATABASE(S): ' + TARGET_DATABASE)
+    log.info(f'TARGET_URL:         {target_url}')
+    log.info(f'TARGET_USERNAME:    {target_username}')
+    log.info(f'TARGET_PASSWORD:    ********')
+    log.info(f'TARGET_DATABASE(S): {target_database}')
 
-    log.info('SOURCE_URL:      ' + SOURCE_URL)
-    log.info('SOURCE_USERNAME: ' + SOURCE_USERNAME)
-    log.info('SOURCE_PASSWORD: ********')
-    log.info('SOURCE_DATABASE: ' + SOURCE_DATABASE)
+    log.info(f'SOURCE_URL:         {source_url}')
+    log.info(f'SOURCE_USERNAME:    {source_username}')
+    log.info(f'SOURCE_PASSWORD:    ********')
+    log.info(f'SOURCE_DATABASE:    {source_database}')
 
-    targets = map(str.strip, TARGET_DATABASE.split(','))
-    sources = map(str.strip, SOURCE_DATABASE.split(','))
+    targets = map(str.strip, target_database.split(','))
+    sources = map(str.strip, source_database.split(','))
 
     for target in targets:
         for source in sources:
             log.info('START SYNC SOURCE (' + source + ') WITH TARGET (' + target + ')')
             this_job = Job(
-                target_url=TARGET_URL,
-                target_email=TARGET_USERNAME,
-                target_password=TARGET_PASSWORD,
+                target_url=target_url,
+                target_email=target_username,
+                target_password=target_password,
                 target_database=target,
-                source_url=SOURCE_URL,
-                source_email=SOURCE_USERNAME,
-                source_password=SOURCE_PASSWORD,
+                source_url=source_url,
+                source_email=source_username,
+                source_password=source_password,
                 source_database=source,
-                job_strategy=JOB_STRATEGY,
+                job_strategy=job_strategy,
             )
             this_job.run_strategy()
             log.info('END SYNC SOURCE (' + source + ') WITH TARGET (' + target + ')')
